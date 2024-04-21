8e76563bc33f2e2a3164a43ff900e806e5404484
TodoBugado <rafaelsouzaaguiar10@gmail.com>
2024-04-21 17:28:33 -0300

    refactor(p1-pdmn): altera componente gerador lista
    - altera o componente LembreteLista pra que o mesmo gere a lista de lembretes
    - altera o componente principal para apenas chamar o LembreteLista

--- app-lembretes/src/LembreteLista.js
+++ app-lembretes/src/LembreteLista.js
@@ -4,11 +4,22 @@ import ReactDOM from 'react-dom'
 export default class LembreteLista extends Component {
   render() {
     return (
-      <div className="flex-grow-1 col-sm-12 col-md-6 col-xl-4 col-xxl-3">
-        <div className="text-bg-primary rounded text-center m-3 p-2">
-          {this.props.lembrete}
-        </div>
+      <div className="row">
+        {
+          this.props.lista[0] ? (
+            this.props.lista.map((lembrete) => (
+              <div className="flex-grow-1 col-sm-12 col-md-6 col-xl-4 col-xxl-3">
+                <div className="text-bg-primary rounded text-center m-3 p-2">
+                  {lembrete}
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="align-items-center text-center m-2">Nenhum lembrete criado</div>
+          )
+        }
       </div>
+
     )
   }
 }
